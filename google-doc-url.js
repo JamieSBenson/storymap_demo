b09de6ce4002c610680bbe3f5f300e2afed54ded
JamieSBenson <jamieseanbenson@gmail.com>
2020-11-18 20:32:16 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZzuTHeN279FrDUQCmf_tHw3HY9vQYId9OXvUZSej2mo/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyCfT6VIHmJdGTMq_2YWZkXmz1i6krEh9XU';
